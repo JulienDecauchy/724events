7706b5fc177fbfb32afe08c45253e7b4243ce341
JulienDecauchy <julien.decauchy1@gmail.com>
2023-09-11 21:43:27 +0200

    Fixed Slider Button on Index

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -45,7 +45,7 @@ const Slider = () => {
                   key={`${event.id}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={radioIdx === index}
                 />
               ))}
             </div>
